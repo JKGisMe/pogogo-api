aeae8177710f0144835d95920f17167bc0162674
JKGisMe <JKGisMe@users.noreply.github.com>
2016-07-31 22:29:07 -0400

    add content-type to allowed headers (#7)

--- lib/pogogo/endpoint.ex
+++ lib/pogogo/endpoint.ex
@@ -3,7 +3,7 @@ defmodule Pogogo.Endpoint do
 
   socket "/socket", Pogogo.UserSocket
 
-  plug Corsica, origins: "*", allow_headers: ["accept"], log: [rejected: :warn, invalid: :warn, accepted: :warn]
+  plug Corsica, origins: "*", allow_headers: ["accept", "content-type"], log: [rejected: :warn, invalid: :warn, accepted: :warn]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
