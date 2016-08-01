d639eb9946ca962a5f7da1c4d3bf0a47707e8695
Kate <(none)>
2016-07-31 21:40:30 -0400

    cors sucks

--- lib/pogogo/endpoint.ex
+++ lib/pogogo/endpoint.ex
@@ -3,7 +3,7 @@ defmodule Pogogo.Endpoint do
 
   socket "/socket", Pogogo.UserSocket
 
-  plug Corsica, origins: "https://obscure-earth-63310.herokuapp.com"
+  plug Corsica, origins: "*"
 
   # Serve at "/" the static files from "priv/static" directory.
   #
